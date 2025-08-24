dd35b9ecefe8d421657bd594583c64bcc59fd782
Pete <38395635+UniquePete@users.noreply.github.com>
2025-08-24 12:52:46 +1000

    Update ZMPT101B.h
    Add definitions option for ASR_Arduino (Heltec CubeCell)
    Add methods to provide flexibility in setting parameters

--- src/ZMPT101B.h
+++ src/ZMPT101B.h
@@ -15,19 +15,30 @@
 #elif defined(ESP32)
 	#define ADC_SCALE 4095.0
 	#define VREF 3.3
+#elif defined(__ASR_Arduino__)
+	#define ADC_SCALE 4095.0
+	#define VREF 2.4
+#else
+	#define ADC_SCALE 4095.0
+	#define VREF 3.3
 #endif
 
 class ZMPT101B
 {
 public:
 	ZMPT101B (uint8_t pin, uint16_t frequency = DEFAULT_FREQUENCY);
+	ZMPT101B (uint8_t pin, float adcVref, uint16_t adcScale, float sensitivity);
 	void     setSensitivity(float value);
 	float 	 getRmsVoltage(uint8_t loopCount = 1);
+	float 	 getRmsVoltage(uint16_t frequency, uint8_t loopCount);
 
 private:
 	uint8_t  pin;
 	uint32_t period;
 	float 	 sensitivity = DEFAULT_SENSITIVITY;
+	uint16_t frequency = DEFAULT_FREQUENCY;
+	float    adcVref;
+	uint16_t adcScale;
 	int 	 getZeroPoint();
 };
 
